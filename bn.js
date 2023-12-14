91e620d9e4e23e2945f86fa58953f2d4467305ec
mecherguirabeb <153821221+mecherguirabeb@users.noreply.github.com>
2023-12-14 11:53:47 +0100

    Add files via upload

---
+++ bn.js
@@ -0,0 +1,22 @@
+function alertt() {
+    alert('inscription validée ')
+}
+
+function payer(){
+let a=document.getElementById('a').value;
+let b=document.getElementById('b').value;
+let c=document.getElementById('c').value;
+let d=document.getElementById('d').value;
+let e=document.getElementById('e').value;
+let f=document.getElementById('f').value;
+if(a.checked|| b.checked || c.checked || d.checked || e.checked || f.checked){
+    alert('achat terminer ;)')}
+else{
+    alert('achat invalid :(')}
+
+
+
+
+
+
+}
